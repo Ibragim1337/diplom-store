3ee9bb84a559c8d25f9e1cb62f351660c0f5d587
Alakparov Ibragim <Alakp_mr11.itstep.edu.az>
2024-03-21 02:15:47 +0400

    first structure and syles for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 
+import styles from '../../styles/Header.module.css';
+import LOGO from '../../images/logo.svg';
+
+
+import { Link } from "react-router-dom";
+import { ROUTES } from "../../utils/routes";
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className={styles.header}>
+      
+      <div className={styles.logo}>
+        <Link to={ROUTES.HOME}>
+          <img src={LOGO} alt="stuff logo" />
+        </Link>
+      </div>
+
+    </div>
   )
 }
 
