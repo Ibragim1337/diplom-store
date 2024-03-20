e476b9c8d1481d2e47db8ecbe0f729d1627f514c
Alakparov Ibragim <Alakp_mr11.itstep.edu.az>
2024-03-21 03:41:23 +0400

    maded header structure, styles and added icons and profile pic

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,9 @@ import React from "react";
 
 import styles from '../../styles/Header.module.css';
 import LOGO from '../../images/logo.svg';
+import AVATAR from '../../images/avatar.jpg';
+import ICON from '../../images/searchIcon.svg';
+import HEART from '../../images/heart.svg'
 
 
 import { Link } from "react-router-dom";
@@ -17,6 +20,46 @@ const Header = () => {
         </Link>
       </div>
 
+      <div className={styles.information}>
+        <div className={styles.user}>
+         <div 
+         className={styles.avatar}
+         style={{ backgroundImage: `url(${AVATAR})` }}
+          />
+          <div className={styles.username}>Guest</div>
+        </div>
+
+        <form className={styles.form}>
+        <div className={styles.icon}>
+          <img 
+          className={styles.searchIcon}
+          src={ICON} 
+          alt="search logo"/>
+        </div>
+
+        <div className={styles.inputContainer}>
+          <input
+          className={styles.input} 
+          type="search" 
+          name="search" 
+          placeholder="Search ..." 
+          autoComplete="off" 
+          onChange={() => {} } 
+          value=''/>
+        </div>
+
+        <div className={styles.box}>
+
+
+        </div>
+        </form>
+        <div className={styles.account}>
+          <Link to={ROUTES.HOME}>
+            <img src={HEART} alt="heart logo" />
+          </Link>
+          
+        </div>
+      </div>
     </div>
   )
 }
