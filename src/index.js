6176faba4105883d84e7c3061ac1df39022a54ce
Alakparov Ibragim <Alakp_mr11.itstep.edu.az>
2024-03-21 01:06:53 +0400

    added files for using react router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
 // import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <App />
-)
-
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
