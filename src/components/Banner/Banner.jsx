d8693c413f7325113eea293763168481b3c8a6c1
Alakparov Ibragim <Alakp_mr11.itstep.edu.az>
2024-03-22 03:03:28 +0400

    some minor changes

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,27 +1,31 @@
 import React from "react";
 
-import styles from '../../styles/Home.module.css';
+import styles from "../../styles/Home.module.css";
 
-import BANNER from '../../images/banner.png'
+import BANNER from "../../images/banner.png";
 
 const Banner = () => {
   return (
     <section className={styles.banner}>
       <div className={styles.left}>
-      <p className={styles.content}>NEW YEAR
-      <span>SALE</span>
-      </p>
-      <button className={styles.more}>See More</button>
+        <p className={styles.content}>
+          NEW YEAR
+          <span>SALE</span>
+        </p>
+        <button className={styles.more}>See More</button>
       </div>
 
-      <div className={styles.right} style={{backgroundImage: `url(${BANNER})` }}>
-
-      <p className={styles.discount}> save up to <span>50%</span> off</p>
-
+      <div
+        className={styles.right}
+        style={{ backgroundImage: `url(${BANNER})` }}
+      >
+        <p className={styles.discount}>
+          {" "}
+          save up to <span>50%</span> off
+        </p>
       </div>
-
     </section>
-  )
-}
+  );
+};
 
 export default Banner;
