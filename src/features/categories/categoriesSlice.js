4a628f0c09eefc1d0707c056de69a5018f73ee6f
Alakparov Ibragim <Alakp_mr11.itstep.edu.az>
2024-03-21 20:00:45 +0400

    connect API and  first logic for categories maybe will be changed in future

--- src/features/categories/categoriesSlice.js
+++ src/features/categories/categoriesSlice.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { BASE_URL } from "../../utils/constatns";
 
 
-export const getCotigories = createAsyncThunk('categories/getCategories', async (_, thunkAPI) => {
+export const getCatigories = createAsyncThunk('categories/getCategories', async (_, thunkAPI) => {
   try{
     const res = await axios(`${BASE_URL}/categories`);
     return res.data;
@@ -18,5 +18,21 @@ categoriesSlice = createSlice({
   name: 'categories',
   initialState: {
     list:[],
-  }
+    isLoading: false
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getCatigories.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(getCatigories.fulfilled, (state, { payload }) => {
+      state.list = payload;
+      state.isLoading = false;
+    });
+    builder.addCase(getCatigories.rejected, (state) => {
+      state.isLoading = false;
+      console.log('Error!');
+    });
+  },
 })
+
+export default categoriesSlice.reducer;
