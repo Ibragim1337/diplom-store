68f411692387239b26820a081065fbd4782eb2f0
Alakparov Ibragim <Alakp_mr11.itstep.edu.az>
2024-03-21 20:24:24 +0400

    api is working

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import React, { useEffect } from "react";
+
+
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import AppRoutes from "./components/Routes/Routes";
 import Sidebar from "./components/Sidebar/Sidebar";
 
+
+import { useDispatch } from "react-redux";
+import { getCatigories } from "./features/categories/categoriesSlice";
+
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCatigories())
+  }, [dispatch])
+
   return (
     <div className="app">
       <Header/>
