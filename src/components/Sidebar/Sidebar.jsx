3659cc0921a0d272e69442dd20ac01736215f083
Alakparov Ibragim <Alakp_mr11.itstep.edu.az>
2024-03-24 01:30:54 +0400

    fixed another bug with API

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,37 +1,52 @@
 import React from "react";
 
-import styles from '../../styles/Sidebar.module.css';
+import styles from "../../styles/Sidebar.module.css";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const Sidebar = () => {
-
   const { list } = useSelector(({ categories }) => categories);
-
-
 
   return (
     <section className={styles.sidebar}>
       <div className={styles.title}> CATEGORIES</div>
       <nav>
         <ul className={styles.menu}>
-          {list.map(({id, name }) => (
-            <li key={id}>
-            <NavLink className={({ isActive }) => `${styles.link} ${isActive ? styles.active : ""}`} to={`/categories/${id}`}>
-             {name}
-            </NavLink>
-          </li>
-          ))}
-
+          {list.map(({ id, name }) => {
+            if (id > 8) return null; // Остановить выполнение map(), если id больше 5
+            return (
+              <li key={id}>
+                <NavLink
+                  className={({ isActive }) =>
+                    `${styles.link} ${isActive ? styles.active : ""}`
+                  }
+                  to={`/categories/${id}`}
+                >
+                  {name}
+                </NavLink>
+              </li>
+            );
+          })}
         </ul>
       </nav>
 
       <div className={styles.footer}>
-        <a href="/help" target="_blank"  className={styles.link}> Help </a>
-        <a href="/terms" target="_blank"  className={styles.link} style={{ textDecoration: 'underline'}}> Term and Conditions </a>
+        <a href="/help" target="_blank" className={styles.link}>
+          {" "}
+          Help{" "}
+        </a>
+        <a
+          href="/terms"
+          target="_blank"
+          className={styles.link}
+          style={{ textDecoration: "underline" }}
+        >
+          {" "}
+          Term and Conditions{" "}
+        </a>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default Sidebar;
