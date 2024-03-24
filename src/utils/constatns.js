fa7642d4e77f4a6e714eda400eecafc25554788d
Alakparov Ibragim <Alakp_mr11.itstep.edu.az>
2024-03-24 23:33:17 +0400

    fixed bug

--- src/utils/constatns.js
+++ src/utils/constatns.js
@@ -1,1 +1,5 @@
 export const BASE_URL = "https://api.escuelajs.co/api/v1";
+
+export const GATEGORY_URL = 'https://api.escuelajs.co/api/v1/categories';
+
+export const PRODUCTS_URL = 'https://api.escuelajs.co/api/v1/products';
