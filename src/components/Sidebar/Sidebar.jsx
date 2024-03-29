a8a32ba7cacfa69e3edb74f3259381c32c04bf6d
Alakparov Ibragim <Alakp_mr11.itstep.edu.az>
2024-03-30 01:25:11 +0400

    smooth scroll to the category

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 
 import styles from "../../styles/Sidebar.module.css";
+import stylesCategory from '../../styles/Category.module.css'
+
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
+
+const handleClick = () => {
+  const wrapper = document.querySelector(`.${stylesCategory.wrapper}`);
+  if (wrapper) {
+    wrapper.scrollIntoView({ behavior: 'smooth' });
+  }
+};
 
 const Sidebar = () => {
   const { list } = useSelector(({ categories }) => categories);
@@ -11,23 +20,24 @@ const Sidebar = () => {
     <section className={styles.sidebar}>
       <div className={styles.title}> CATEGORIES</div>
       <nav>
-        <ul className={styles.menu}>
-        {list.map(({ id, name }) => {
-            if (id > 6) return null; 
-            return (
-              <li key={id}>
-                <NavLink
-                  className={({ isActive }) =>
-                    `${styles.link} ${isActive ? styles.active : ""}`
-                  }
-                  to={`/categories/${id}`}
-                >
-                  {name}
-                </NavLink>
-              </li>
-            );
-          })}
-        </ul>
+      <ul className={styles.menu}>
+      {list.map(({ id, name }) => {
+        if (id > 6) return null; 
+        return (
+          <li key={id}>
+            <NavLink
+              className={({ isActive }) =>
+                `${styles.link} ${isActive ? styles.active : ""}`
+              }
+              to={`/categories/${id}`}
+              onClick={handleClick}
+            >
+              {name}
+            </NavLink>
+          </li>
+        );
+      })}
+    </ul>
       </nav>
 
       
